d71d4cd530f7815e64091b415f4246e912e19665
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-01 23:51:43 +0500

    variable practice

---
+++ main.js
@@ -0,0 +1,9 @@
+var message1 = "Hey";
+console.log(message1);
+message1 = "Hello";
+console.log(message1);
+var firstname = "Mehjabin";
+console.log(firstname);
+//Now using camelcase;
+var firstName1 = "Mehjabin";
+console.log(firstName1);
---
+++ main.ts
@@ -0,0 +1,9 @@
+let message1 = "Hey";
+console.log(message1);
+message1 = "Hello";
+console.log(message1);
+let firstname = "Mehjabin";
+console.log(firstname);
+//Now using camelcase;
+let firstName1 = "Mehjabin";
+console.log(firstName1);
